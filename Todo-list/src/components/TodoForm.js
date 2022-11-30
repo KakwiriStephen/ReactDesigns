f9d5bac73cc59d43c0687a9c8f1b960ef48bbdfa
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-30 18:02:40 +0300

    Form & Button

--- Todo-list/src/components/TodoForm.js
+++ Todo-list/src/components/TodoForm.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function TodoForm() {
   return (
     <div>
-        
+        <form className='todo-form'>
+            <input className='todo-input' placeholder='Add todo' />
+            <button className='todo-button'>Add Todo</button>
+        </form>
+
     </div>
   )
 }

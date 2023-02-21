514f86d3b4ad3546a94062b201db08259f712cb6
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-21 22:01:03 +0300

    Add By

--- react-redux/src/store/store.js
+++ react-redux/src/store/store.js
@@ -11,7 +11,8 @@ const reducerFn = (state = { counter: 0 }, action) => {
   if (action.type === "DEC") {
     return { counter: state.counter - 1 };
   }
-
+  if (action.type === "ADD") {
+  }
   return state;
 };
 

0f73f944bbac7643be5bdf3dade0acadf4085512
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-21 17:45:13 +0300

    Bugs

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+
   return (
     <div>
       <h1>Counter App</h1>
+      <h2>{counter}</h2>
     </div>
   );
 }

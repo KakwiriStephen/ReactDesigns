482c9dedcb3b985a3e5da3ef068a96800d94ad0a
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-07 18:23:44 +0300

    Bug

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -7,6 +7,8 @@ import { useSelector } from "react-redux";
 function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   console.log(isLoggedIn);
+  const cartItems = useSelector((state) => state.cart.ItemsList);
+  console.log(cartItems);
 
   return (
     <div className="App">

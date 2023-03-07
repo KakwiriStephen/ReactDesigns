0e93328f38390429646cc6d088bf17ce22e88ee1
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-07 17:31:10 +0300

    AddToCart

--- shopping/src/store/cart-slice.js
+++ shopping/src/store/cart-slice.js
@@ -8,7 +8,25 @@ const cartSlice = createSlice({
     showCart: false,
   },
   reducers: {
-    addToCart() {},
+    addToCart(state, action) {
+      const newItem = action.payload;
+
+      //Check if the item is alredy available
+      const existingItem = state.itemsList.find(
+        (item) => item.id === newItem.id
+      );
+
+      if (existingItem) {
+        existingItem.quantity++;
+        existingItem.price += newItem.price;
+      } else {
+        state.itemsList.push({
+          id: newItem.id,
+          price: newItem.price,
+          name: newItem,
+        });
+      }
+    },
     removeFromCart() {},
     setShowCart(state) {
       state.showCart = true;
